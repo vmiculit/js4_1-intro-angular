9cb65e953c23e14b15d6412549d0481c72284451
Valentin Miculit <valsbox@gmail.com>
2016-01-21 10:59:50 -0700

    completed

--- part3/main.js
+++ part3/main.js
@@ -4,4 +4,14 @@ angular.module("jsIntro",[])
 angular.module("jsIntro")
 	.controller("partThreeTroller",["$scope", function($scope) {
 
+		$scope.popItSwitch = false
+
+		$scope.switchMyPopup = function() {
+			$scope.popItSwitch = true
+		}
+
+		$scope.shutItDown = function() {
+			$scope.popItSwitch = false
+		}
+
 }])
